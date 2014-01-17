ed1b20e872d7659ca60bc011742093271068479f
James Hamet <jameshamet@gmail.com>
2014-01-16 21:53:20 -0500

    remove old files

--- _misc/DD/timetest.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file and all of its containing script is property of James Hamet **********
-
-header( 'Location: http://darkdreams.zzl.org/' );
-
-die();
-
-?>
--- _misc/_url.php
+++
@@ -1,7 +0,0 @@
-<?php
-function getURL(){
-	$s = empty($_SERVER["HTTPS"]) ? '' : ($_SERVER["HTTPS"] == "on") ? "s" : "";
-	$protocol = substr(strtolower($_SERVER["SERVER_PROTOCOL"]), 0, strpos(strtolower($_SERVER["SERVER_PROTOCOL"]), "/")) . $s;
-	$port = ($_SERVER["SERVER_PORT"] == "80") ? "" : (":".$_SERVER["SERVER_PORT"]);
-	return $protocol . "://" . $_SERVER['SERVER_NAME'] . $port . $_SERVER['REQUEST_URI'];
-}
